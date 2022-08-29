0c486cb093f8fdad2bf0f644d4db74bebb197a64
eevaain <evanlin416@gmail.com>
2022-08-29 19:46:27 -0400

    removed an apostrophe

--- pages/Epiphets.js
+++ pages/Epiphets.js
@@ -9,7 +9,7 @@ function Epiphets() {
             <Header/>
         </div>
             <div className="pt-10 flex flex-col place-self-center">
-                <a className="regular">Don't tell your plans. Show the result.</a>
+                <a className="regular">Dont tell your plans. Show the result.</a>
                 <a className="regular">Drop the name-dropping. You are powerful enough on your own.</a>
                 <a className="regular">Start thinking less about yourself and not less of yourself.</a>
                 <a className="regular">The ones who got rich werent the gold miners, but the ones selling the shovels.</a>
